30f9dcd2cd712e1462d18c3db9e6ac11f89257ce
bitfriend <bitfriend@hotmail.com>
2021-03-20 02:40:22 +0800

    Implement smart contracts

---
+++ contracts/DappToken.sol
@@ -0,0 +1,51 @@
+pragma solidity ^0.4.17;
+
+contract DappToken {
+  string public name = 'DApp Token';
+  string public symbol = 'DAPP';
+  string public standard = 'DApp Token v1.0';
+  uint8 public decimals = 18; // same value as wei
+  uint256 public totalSupply;
+
+  mapping(address => uint256) public balanceOf;
+  mapping(address => mapping(address => uint256)) public allowance;
+
+  event Transfer(address _from, address _to, uint256 _value);
+  event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+
+  constructor(uint256 _initialSupply) public {
+    balanceOf[msg.sender] = _initialSupply;
+    totalSupply = _initialSupply;
+    // TODO: Handle fractional tokens
+    // TODO: Trigger a transfer event when deploying
+  }
+
+  function transfer(address _to, uint256 _value) public returns (bool) {
+    require(balanceOf[msg.sender] >= _value);
+
+    balanceOf[msg.sender] -= _value;
+    balanceOf[_to] += _value;
+
+    emit Transfer(msg.sender, _to, _value);
+    return true;
+  }
+
+  function approve(address _spender, uint256 _value) public returns (bool) {
+    allowance[msg.sender][_spender] = _value;
+
+    emit Approval(msg.sender, _spender, _value);
+    return true;
+  }
+
+  function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
+    require(_value <= balanceOf[_from]);
+    require(_value <= allowance[_from][msg.sender]);
+
+    balanceOf[_from] -= _value;
+    balanceOf[_to] += _value;
+    allowance[_from][msg.sender] -= _value;
+
+    emit Transfer(_from, _to, _value);
+    return true;
+  }
+}
